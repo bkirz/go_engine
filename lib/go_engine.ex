1391b282718f173d02261c6cc9e0da36993ee540
Ben Kirzhner <ben.kirzhner@gmail.com>
2020-09-15 08:18:40 -0700

    Initial commit

---
+++ lib/go_engine.ex
@@ -0,0 +1,103 @@
+defmodule GoEngine do
+  @type player() :: :black | :white
+
+  @type move() :: {player(), move_action()}
+  @type move_action() :: :pass | {:place_stone, coordinate()}
+  @type coordinate() :: {pos_integer(), pos_integer()}
+
+  defmodule Board do
+    fields = [:size, :stone_locations]
+    @enforce_keys fields
+    defstruct fields
+
+    @type t() :: %__MODULE__{
+            size: pos_integer(),
+            stone_locations: %{optional(GoEngine.coordinate()) => GoEngine.player()}
+          }
+
+    @stone_chars %{black: "●", white: "○"}
+
+    @spec format(t()) :: String.t()
+    def format(%__MODULE__{size: size, stone_locations: stone_locations}) do
+      Enum.map(1..size, fn row ->
+        1..size
+        |> Enum.map(fn col ->
+          coord = {row, col}
+
+          case Map.fetch(stone_locations, coord) do
+            {:ok, player} -> @stone_chars[player]
+            :error -> empty_board_char_for(size, {row, col})
+          end
+        end)
+        |> Kernel.++(["\n"])
+        |> Enum.join()
+      end)
+      |> Enum.join()
+    end
+
+    defp empty_board_char_for(_size, {1, 1}), do: "┌"
+    defp empty_board_char_for(size, {1, size}), do: "┐"
+    defp empty_board_char_for(size, {size, 1}), do: "└"
+    defp empty_board_char_for(size, {size, size}), do: "┘"
+    defp empty_board_char_for(_size, {1, _}), do: "┬"
+    defp empty_board_char_for(_size, {_, 1}), do: "├"
+    defp empty_board_char_for(size, {size, _}), do: "┴"
+    defp empty_board_char_for(size, {_, size}), do: "┤"
+    defp empty_board_char_for(_, _), do: "┼"
+  end
+
+  defmodule GameState do
+    @type turn_state() ::
+            {:active, GoEngine.player()}
+            | {:active_after_pass, GoEngine.player()}
+            | :complete
+
+    @type t() :: %__MODULE__{
+            board: Board.t(),
+            turn_state: turn_state(),
+            prisoners_captured: %{white: integer(), black: integer()}
+          }
+
+    fields = [:board, :turn_state, :prisoners_captured]
+    @enforce_keys fields
+    defstruct fields
+  end
+
+  @spec new_game_state(board_size: integer()) :: GameState.t()
+  def new_game_state(board_size: board_size) do
+    %GameState{
+      board: %Board{
+        size: board_size,
+        stone_locations: %{}
+      },
+      turn_state: {:active, :black},
+      prisoners_captured: %{white: 0, black: 0}
+    }
+  end
+
+  @spec play_move(GameState.t(), move()) :: GameState.t()
+  def play_move(%GameState{board: %Board{size: size}}, {_, {:place_stone, {row, col}}})
+      when row > size or col > size or row < 1 or col < 1,
+      do: {:invalid, :out_of_bounds}
+
+  def play_move(
+        game_state = %GameState{turn_state: {_, active_player}},
+        {active_player, {:place_stone, at}}
+      ) do
+    updated_state =
+      game_state
+      |> Map.update!(:board, fn board ->
+        %{board | stone_locations: Map.put(board.stone_locations, at, active_player)}
+      end)
+      |> Map.put(:turn_state, {:active, next_player(active_player)})
+
+    {:valid, updated_state}
+  end
+
+  def play_move(%GameState{turn_state: {_, active_player}}, {inactive_player, _}) do
+    {:invalid, :out_of_turn}
+  end
+
+  defp next_player(:black), do: :white
+  defp next_player(:white), do: :black
+end
---
+++ test/go_engine_test.exs
@@ -0,0 +1,88 @@
+defmodule GoEngineTest do
+  use ExUnit.Case
+  doctest GoEngine
+
+  @tag :pending
+  test "supports advancing the game state" do
+    initial_state = GoEngine.new_game_state(board_size: 5)
+
+    assert %GoEngine.GameState{
+             turn_state: {:active, :black},
+             board: %GoEngine.Board{stone_locations: %{}, size: 5}
+           } = initial_state
+
+    {:valid, turn_1_state} = GoEngine.play_move(initial_state, {:black, {:place_stone, {1, 1}}})
+
+    assert %GoEngine.GameState{
+             turn_state: {:active, :white},
+             board: %GoEngine.Board{stone_locations: %{{1, 1} => :black}}
+           } = turn_1_state
+
+    {:valid, turn_2_state} = GoEngine.play_move(turn_1_state, {:white, {:place_stone, {3, 1}}})
+    assert turn_2_state.board.stone_locations == %{{1, 1} => :black, {3, 1} => :white}
+  end
+
+  test "formats an empty board" do
+    expected_board = """
+    ┌┬┬┬┐
+    ├┼┼┼┤
+    ├┼┼┼┤
+    ├┼┼┼┤
+    └┴┴┴┘
+    """
+
+    formatted_board =
+      [board_size: 5]
+      |> GoEngine.new_game_state()
+      |> Map.fetch!(:board)
+      |> GoEngine.Board.format()
+
+    assert expected_board == formatted_board
+  end
+
+  test "formats a board with some stones on it" do
+    state =
+      with state <- GoEngine.new_game_state(board_size: 5),
+           {:valid, state} <-
+             GoEngine.play_move(state, {:black, {:place_stone, {1, 1}}}),
+           {:valid, state} <-
+             GoEngine.play_move(state, {:white, {:place_stone, {3, 4}}}),
+           {:valid, state} <-
+             GoEngine.play_move(state, {:black, {:place_stone, {5, 4}}}),
+           do: state
+
+    expected_board = """
+    ●┬┬┬┐
+    ├┼┼┼┤
+    ├┼┼○┤
+    ├┼┼┼┤
+    └┴┴●┘
+    """
+
+    formatted_board =
+      state
+      |> Map.fetch!(:board)
+      |> GoEngine.Board.format()
+
+    assert expected_board == formatted_board
+  end
+
+  test "rejects moves out of turn" do
+    initial_state = GoEngine.new_game_state(board_size: 5)
+
+    assert {:invalid, :out_of_turn} =
+             GoEngine.play_move(initial_state, {:white, {:place_stone, {1, 1}}})
+  end
+
+  test "rejects placing stones out of bounds" do
+    initial_state = GoEngine.new_game_state(board_size: 5)
+
+    assert {:invalid, :out_of_bounds} =
+             GoEngine.play_move(initial_state, {:black, {:place_stone, {0, 0}}})
+  end
+
+  # test "rejects placing on existing stones"
+  # test "rejects suicidal plays"
+
+  # test "removes captured stones"
+end
